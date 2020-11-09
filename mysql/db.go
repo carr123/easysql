19352259418938158f34ef3ebd692612e982de7a
thatcat <thatcat@sina.com>
2020-11-09 22:00:53 +0800

    sql type

---
+++ mysql/db.go
@@ -0,0 +1,281 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"encoding/base64"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+
+	_ "github.com/Go-SQL-Driver/MySQL"
+	"github.com/jmoiron/sqlx"
+)
+
+type DBServer struct {
+	db *sqlx.DB
+}
+
+type Conn struct {
+	db     *sqlx.DB
+	tx     *sqlx.Tx
+	excter execAndQuery
+}
+
+type QItem map[string]interface{}
+type QArray []QItem
+
+type execAndQuery interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Queryx(query string, args ...interface{}) (*sqlx.Rows, error)
+	Select(dest interface{}, query string, args ...interface{}) error
+}
+
+//"root:123456@tcp(127.0.0.1:3306)/eshop?charset=utf8"
+func New(dataSourceName string, MaxIdleConn int) (*DBServer, error) {
+	inst := &DBServer{}
+	db, err := sqlx.Connect("mysql", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxIdleConns(MaxIdleConn)
+	db.SetConnMaxLifetime(time.Second * 10)
+	inst.db = db
+	return inst, nil
+}
+
+func (this *DBServer) Close() {
+	if this.db != nil {
+		this.db.Close()
+		this.db = nil
+	}
+}
+
+func (this *DBServer) NewConn() *Conn {
+	return &Conn{db: this.db, tx: nil, excter: this.db}
+}
+
+//mysql的事务不会重试，只会执行一次
+func (this *DBServer) ExecInTx(fn func(*Conn) error) error {
+	ctx := context.Background()
+	tx, err := this.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	conn := &Conn{db: this.db, tx: tx, excter: tx}
+	if err := fn(conn); err == nil {
+		return tx.Commit()
+	} else {
+		tx.Rollback()
+		return err
+	}
+}
+
+//insert update delete
+//create table, alter index etc.
+func (this *Conn) Exec(cmd string, args ...interface{}) error {
+	query, argsx, err := sqlx.In(cmd, args...)
+	if err != nil {
+		return err
+	}
+
+	query = this.db.Rebind(query)
+	_, err = this.excter.Exec(query, argsx...)
+	return err
+}
+
+//insert many records at one shot. often insert many logs.
+//values := make([]interface{}, 0, batchsize*nCol)
+//conn.BulkInsert("insert into files(bucket,filename)", nCol, values...)
+func (this *Conn) BulkInsert(cmd string, nCol int, args ...interface{}) error {
+	var szSQL string
+	szBracket := "(" + strings.TrimSuffix(strings.Repeat("?,", nCol), ",") + "),"
+	szSQL = cmd + " values " + strings.TrimSuffix(strings.Repeat(szBracket, len(args)/nCol), ",")
+	return this.Exec(szSQL, args...)
+}
+
+func MakeQArray() QArray {
+	return make(QArray, 0)
+}
+
+//query database
+func (this *Conn) Query(query string, args ...interface{}) (QArray, error) {
+	queryx, argsx, err := sqlx.In(query, args...)
+	if err != nil {
+		return nil, err
+	}
+	queryx = this.db.Rebind(queryx)
+
+	rows, err := this.excter.Queryx(queryx, argsx...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	result := make(QArray, 0, 100)
+	for rows.Next() {
+		out := make(map[string]interface{})
+		err := rows.MapScan(out)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, out)
+	}
+
+	return result, nil
+}
+
+//query database
+func (this *Conn) Select(dest interface{}, query string, args ...interface{}) error {
+	queryx, argsx, err := sqlx.In(query, args...)
+	if err != nil {
+		return err
+	}
+	queryx = this.db.Rebind(queryx)
+	return this.excter.Select(dest, queryx, argsx...)
+}
+
+//select count(*) from ...
+func (this *Conn) QueryCount(query string, args ...interface{}) (int64, error) {
+	queryx, argsx, err := sqlx.In(query, args...)
+	if err != nil {
+		return 0, err
+	}
+	queryx = this.db.Rebind(queryx)
+
+	rows, err := this.excter.Queryx(queryx, argsx...)
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+
+	var count int64 = 0
+	for rows.Next() {
+		err := rows.Scan(&count)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return count, nil
+}
+
+func (this *Conn) Close() error {
+	return nil
+}
+
+func (item QItem) AsMap() map[string]interface{} {
+	return item
+}
+
+func (item QItem) GetColumnString(key string) string {
+	return item[key].(string)
+}
+
+func (item QItem) GetColumnInt64(key string) int64 {
+	return item[key].(int64)
+}
+
+func (item QItem) GetColumnFloat64(key string) float64 {
+	return item[key].(float64)
+}
+
+func (item QItem) ToInt64(keys ...string) QItem {
+	for _, key := range keys {
+		if ss, ok := item[key].(string); ok {
+			item[key], _ = strconv.ParseInt(ss, 10, 64)
+		}
+	}
+	return item
+}
+
+func (item QItem) IntToBool(keys ...string) QItem {
+	for _, key := range keys {
+		if ss, ok := item[key].(string); ok {
+			n, _ := strconv.ParseInt(ss, 10, 64)
+			if n == 0 {
+				item[key] = false
+			} else {
+				item[key] = true
+			}
+		}
+	}
+	return item
+}
+
+func (item QItem) ToFloat64(keys ...string) QItem {
+	for _, key := range keys {
+		if ss, ok := item[key].(string); ok {
+			item[key], _ = strconv.ParseFloat(ss, 64)
+		}
+	}
+	return item
+}
+
+func (item QItem) Base64Encode(keys ...string) QItem {
+	for _, key := range keys {
+		if ss, ok := item[key].(string); ok {
+			item[key] = base64.StdEncoding.EncodeToString([]byte(ss))
+		}
+	}
+	return item
+}
+
+func (array *QArray) ToInt64(key ...string) *QArray {
+	for _, item := range *array {
+		item.ToInt64(key...)
+	}
+	return array
+}
+
+func (array *QArray) Shuffle() *QArray {
+	nLen := len(*array)
+
+	if nLen < 2 {
+		return array
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	dest := make([]QItem, nLen)
+	perm := rand.Perm(nLen)
+	for i, v := range perm {
+		dest[v] = (*array)[i]
+	}
+
+	*array = dest
+
+	return array
+}
+
+func (array *QArray) IntToBool(key ...string) *QArray {
+	for _, item := range *array {
+		item.IntToBool(key...)
+	}
+	return array
+}
+
+func (array *QArray) ToFloat64(key ...string) *QArray {
+	for _, item := range *array {
+		item.ToFloat64(key...)
+	}
+	return array
+}
+
+func (array *QArray) Base64Encode(key ...string) *QArray {
+	for _, item := range *array {
+		item.Base64Encode(key...)
+	}
+	return array
+}
+
+func (array *QArray) ToRawArray() []interface{} {
+	arr := make([]interface{}, 0, len(*array))
+
+	for _, item := range *array {
+		arr = append(arr, map[string]interface{}(item))
+	}
+
+	return arr
+}
---
+++ types.go
@@ -0,0 +1,523 @@
+package easysql
+
+import (
+	"bytes"
+	"database/sql"
+	"database/sql/driver"
+	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/lib/pq"
+)
+
+/*
+type STRING = easysql.STRING
+type INT64 = easysql.INT64
+type DATE = easysql.DATE
+type DATETIME = easysql.DATETIME
+type FLOAT64 = easysql.FLOAT64
+type BOOL = easysql.BOOL
+type JSONB = easysql.JSONB
+type StringArray = easysql.StringArray
+type Int64Array = easysql.Int64Array
+*/
+
+//定义的数据类型能够接收数据库的空字段。空字段被解析为默认类型。比如 "", 0, false
+//------------------------------------------------------------------------------
+
+//日期类型为字符串，格式为: 2006-01-02
+//空字段解析为空字符串
+type DATE struct {
+	sql.NullString
+}
+
+func NewDate(szDate string) DATE {
+	obj := DATE{}
+	obj.NullString.Valid = false
+	if len(szDate) > 0 {
+		if _, err := time.Parse("2006-01-02", szDate); err == nil {
+			obj.NullString.String = szDate
+			obj.NullString.Valid = true
+		}
+	}
+	return obj
+}
+
+// Scan implements the Scanner interface.
+func (t *DATE) Scan(value interface{}) error {
+	if err := t.NullString.Scan(value); err != nil {
+		return err
+	}
+
+	if t.NullString.Valid && len(t.NullString.String) > 10 {
+		t.NullString.String = t.NullString.String[0:10]
+	}
+
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (t DATE) Value() (driver.Value, error) {
+	if !t.NullString.Valid {
+		return nil, nil
+	}
+	return t.NullString.String, nil
+}
+
+func (t DATE) MarshalJSON() ([]byte, error) {
+	if t.NullString.Valid {
+		return json.Marshal(t.NullString.String)
+	} else {
+		return json.Marshal("")
+	}
+}
+
+func (t DATE) MarshalText() ([]byte, error) {
+	return []byte(t.String()), nil
+}
+
+func (t DATE) String() string {
+	if t.NullString.Valid {
+		return t.NullString.String
+	} else {
+		return ""
+	}
+}
+
+//格式为: "2006-01-02"
+func (t *DATE) SetVal(szDate string) error {
+	if len(szDate) > 0 {
+		if _, err := time.Parse("2006-01-02", szDate); err == nil {
+			t.NullString.String = szDate
+			t.NullString.Valid = true
+			return nil
+		} else {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (t *DATE) SetNULL() {
+	t.NullString.String = ""
+	t.NullString.Valid = false
+}
+
+func (t *DATE) UnmarshalJSON(data []byte) error {
+	var ss string
+	if err := json.Unmarshal(data, &ss); err != nil {
+		return err
+	}
+
+	tt, err := time.Parse("2006-01-02", ss)
+	if err != nil {
+		return err
+	}
+
+	t.NullString.Valid = true
+	t.NullString.String = tt.Format("2006-01-02")
+
+	return nil
+}
+
+//------------------------------------------------------------------------------
+//时间日期为字符串，格式为: "2006-01-02 15:04:05"
+//空字段解析为空字符串
+type DATETIME struct {
+	sql.NullString
+}
+
+func NewDateTime(szDate string) DATETIME {
+	obj := DATETIME{}
+	obj.NullString.Valid = false
+
+	if len(szDate) > 0 {
+		if _, err := time.Parse("2006-01-02 15:04:05", szDate); err == nil {
+			obj.NullString.String = szDate
+			obj.NullString.Valid = true
+		}
+	}
+
+	return obj
+}
+
+// Scan implements the Scanner interface.
+func (t *DATETIME) Scan(value interface{}) error {
+	if err := t.NullString.Scan(value); err != nil {
+		return err
+	}
+
+	if t.NullString.Valid {
+		ss := strings.ReplaceAll(t.NullString.String, "T", " ")
+		ss = strings.ReplaceAll(ss, "Z", "")
+		t.NullString.String = ss
+	}
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (t DATETIME) Value() (driver.Value, error) {
+	if !t.NullString.Valid {
+		return nil, nil
+	}
+	return t.NullString.String, nil
+}
+
+func (t DATETIME) MarshalJSON() ([]byte, error) {
+	if t.NullString.Valid {
+		return json.Marshal(t.NullString.String)
+	} else {
+		return json.Marshal("")
+	}
+}
+
+func (t DATETIME) MarshalText() ([]byte, error) {
+	return []byte(t.String()), nil
+}
+
+func (t DATETIME) String() string {
+	if t.NullString.Valid {
+		return t.NullString.String
+	} else {
+		return ""
+	}
+}
+
+func (t *DATETIME) UnmarshalJSON(data []byte) error {
+	var ss string
+	if err := json.Unmarshal(data, &ss); err != nil {
+		return err
+	}
+
+	tt, err := time.Parse("2006-01-02 15:04:05", ss)
+	if err != nil {
+		return err
+	}
+
+	t.NullString.Valid = true
+	t.NullString.String = tt.Format("2006-01-02 15:04:05")
+
+	return nil
+}
+
+//格式为: "2006-01-02 15:04:05"
+func (t *DATETIME) SetVal(szDateTime string) error {
+	if len(szDateTime) > 0 {
+		if _, err := time.Parse("2006-01-02 15:04:05", szDateTime); err == nil {
+			t.NullString.String = szDateTime
+			t.NullString.Valid = true
+			return nil
+		} else {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (t *DATETIME) SetNULL() {
+	t.NullString.String = ""
+	t.NullString.Valid = false
+}
+
+//------------------------------------------------------------------------------
+//空字段解析为空字符串
+type STRING struct {
+	sql.NullString
+}
+
+// Scan implements the Scanner interface.
+func (t *STRING) Scan(value interface{}) error {
+	return t.NullString.Scan(value)
+}
+
+// Value implements the driver Valuer interface.
+func (t STRING) Value() (driver.Value, error) {
+	return t.NullString.Value()
+}
+
+func (t STRING) MarshalJSON() ([]byte, error) {
+	if t.NullString.Valid {
+		return json.Marshal(t.NullString.String)
+	} else {
+		return json.Marshal("")
+	}
+}
+
+func (t *STRING) UnmarshalJSON(data []byte) error {
+	var ss string
+	if err := json.Unmarshal(data, &ss); err != nil {
+		return err
+	}
+
+	t.NullString.Valid = true
+	t.NullString.String = ss
+
+	return nil
+}
+
+func (t STRING) String() string {
+	if t.NullString.Valid {
+		return t.NullString.String
+	} else {
+		return ""
+	}
+}
+
+func (t *STRING) SetVal(szVal string) error {
+	t.NullString.String = szVal
+	t.NullString.Valid = true
+	return nil
+}
+
+func (t *STRING) SetNULL() {
+	t.NullString.String = ""
+	t.NullString.Valid = false
+}
+
+//------------------------------------------------------------------------------
+//空字段会解析为0
+type INT64 struct {
+	sql.NullInt64
+}
+
+// Scan implements the Scanner interface.
+func (t *INT64) Scan(value interface{}) error {
+	return t.NullInt64.Scan(value)
+}
+
+// Value implements the driver Valuer interface.
+func (t INT64) Value() (driver.Value, error) {
+	return t.NullInt64.Value()
+}
+
+func (t INT64) MarshalJSON() ([]byte, error) {
+	if t.NullInt64.Valid {
+		return json.Marshal(t.NullInt64.Int64)
+	} else {
+		return json.Marshal(0)
+	}
+}
+
+func (t *INT64) UnmarshalJSON(data []byte) error {
+	if len(data) == 0 {
+		return nil
+	}
+
+	var nNum int64
+	if err := json.Unmarshal(data, &nNum); err != nil {
+		return err
+	}
+
+	t.NullInt64.Valid = true
+	t.NullInt64.Int64 = nNum
+
+	return nil
+}
+
+func (t *INT64) SetVal(val int64) error {
+	t.NullInt64.Int64 = val
+	t.NullInt64.Valid = true
+	return nil
+}
+
+func (t *INT64) SetNULL() {
+	t.NullInt64.Int64 = 0
+	t.NullInt64.Valid = false
+}
+
+//------------------------------------------------------------------------------
+//空字段会解析为0
+type FLOAT64 struct {
+	sql.NullFloat64
+}
+
+// Scan implements the Scanner interface.
+func (t *FLOAT64) Scan(value interface{}) error {
+	return t.NullFloat64.Scan(value)
+}
+
+// Value implements the driver Valuer interface.
+func (t FLOAT64) Value() (driver.Value, error) {
+	return t.NullFloat64.Value()
+}
+
+func (t FLOAT64) MarshalJSON() ([]byte, error) {
+	if t.NullFloat64.Valid {
+		return json.Marshal(t.NullFloat64.Float64)
+	} else {
+		return json.Marshal(0)
+	}
+}
+
+func (t *FLOAT64) UnmarshalJSON(data []byte) error {
+	if len(data) == 0 {
+		return nil
+	}
+
+	var nNum float64
+	if err := json.Unmarshal(data, &nNum); err != nil {
+		return err
+	}
+
+	t.NullFloat64.Valid = true
+	t.NullFloat64.Float64 = nNum
+
+	return nil
+}
+
+func (t *FLOAT64) SetVal(val float64) error {
+	t.NullFloat64.Float64 = val
+	t.NullFloat64.Valid = true
+	return nil
+}
+
+func (t *FLOAT64) SetNULL() {
+	t.NullFloat64.Float64 = 0
+	t.NullFloat64.Valid = false
+}
+
+//------------------------------------------------------------------------------
+//空字段会解析为 false
+type BOOL struct {
+	sql.NullBool
+}
+
+// Scan implements the Scanner interface.
+func (t *BOOL) Scan(value interface{}) error {
+	return t.NullBool.Scan(value)
+}
+
+// Value implements the driver Valuer interface.
+func (t BOOL) Value() (driver.Value, error) {
+	return t.NullBool.Value()
+}
+
+func (t BOOL) MarshalJSON() ([]byte, error) {
+	if t.NullBool.Valid {
+		return json.Marshal(t.NullBool.Bool)
+	} else {
+		return json.Marshal(false)
+	}
+}
+
+func (t *BOOL) UnmarshalJSON(data []byte) error {
+	if len(data) == 0 {
+		return nil
+	}
+
+	var val bool
+	if err := json.Unmarshal(data, &val); err != nil {
+		return err
+	}
+
+	t.NullBool.Valid = true
+	t.NullBool.Bool = val
+	return nil
+}
+
+func (t *BOOL) SetVal(val bool) error {
+	t.NullBool.Bool = val
+	t.NullBool.Valid = true
+	return nil
+}
+
+func (t *BOOL) SetNULL() {
+	t.NullBool.Bool = false
+	t.NullBool.Valid = false
+}
+
+//------------------------------------------------------------------------------
+//JSONB格式，数据库字段格式为: "{}", 不能为json数组等其他格式
+//空字段解析为空map
+type JSONB struct {
+	KV map[string]interface{}
+}
+
+// Scan implements the Scanner interface.
+func (t *JSONB) Scan(value interface{}) error {
+	var ss sql.NullString
+
+	if err := ss.Scan(value); err != nil {
+		return err
+	}
+
+	if ss.Valid {
+		d := json.NewDecoder(strings.NewReader(ss.String))
+		d.UseNumber()
+		if err := d.Decode(&t.KV); err != nil {
+			return err
+		}
+		return nil
+
+	} else {
+		t.KV = make(map[string]interface{})
+	}
+
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (t JSONB) Value() (driver.Value, error) {
+	if t.KV == nil {
+		return nil, nil
+	}
+
+	bin, err := json.Marshal(t.KV)
+	if err != nil {
+		return nil, err
+	}
+
+	return string(bin), nil
+}
+
+func (t JSONB) MarshalJSON() ([]byte, error) {
+	if t.KV == nil {
+		return json.Marshal(map[string]interface{}{})
+	} else {
+		return json.Marshal(t.KV)
+	}
+}
+
+func (t JSONB) MarshalText() ([]byte, error) {
+	return t.MarshalJSON()
+}
+
+func (t JSONB) String() string {
+	if bin, err := t.MarshalJSON(); err == nil {
+		return string(bin)
+	} else {
+		return "{}"
+	}
+}
+
+func (t *JSONB) UnmarshalJSON(data []byte) error {
+	d := json.NewDecoder(bytes.NewReader(data))
+	d.UseNumber()
+	return d.Decode(&t.KV)
+}
+
+func (t *JSONB) Merge(mp map[string]interface{}) *JSONB {
+	for k, v := range mp {
+		t.KV[k] = v
+	}
+
+	return t
+}
+
+func (t *JSONB) SetVal(mp map[string]interface{}) error {
+	t.KV = mp
+	return nil
+}
+
+func (t *JSONB) SetNULL() {
+	t.KV = nil
+}
+
+type StringArray = pq.StringArray
+type Int64Array = pq.Int64Array
+type Float64Array = pq.Float64Array
+type ByteArray = pq.ByteaArray
+type BoolArray = pq.BoolArray
